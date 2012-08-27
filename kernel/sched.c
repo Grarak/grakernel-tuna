eb358f28942de58c1cd3516c817d4f9c5320a758
faux123 <reioux@gmail.com>
2012-08-27 08:20:53 -0700

    sched: implement usage tracking
    From: Paul Turner <pjt@google.com>
    
    With the frame-work for runnable tracking now fully in place.  Per-entity usage
    tracking is a simple and low-overhead addition.
    
    Signed-off-by: Paul Turner <pjt@google.com>
    Reviewed-by: Ben Segall <bsegall@google.com>
    backported from Linux 3.5+ to Linux 3.0 by faux123

--- kernel/sched.c
+++ kernel/sched.c
@@ -276,7 +276,7 @@ struct task_group {
 
 	atomic_t load_weight;
 	atomic64_t load_avg;
-	atomic_t runnable_avg;
+	atomic_t runnable_avg, usage_avg;
 #endif
 
 #ifdef CONFIG_RT_GROUP_SCHED
@@ -364,7 +364,7 @@ struct cfs_rq {
 	atomic64_t decay_counter, removed_load;
 	u64 last_decay;
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	u32 tg_runnable_contrib;
+	u32 tg_runnable_contrib, tg_usage_contrib;
 	u64 tg_load_contrib;
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 
