32b3f844f8df23930af8e6da2352dda151a1cddf
faux123 <reioux@gmail.com>
2012-10-10 18:49:40 -0500

    Revert "mm: Wait for writeback when grabbing pages to begin a write"
    This reverts commit 3d08bcc887a1c8d12be8d81f747ffa2e8a44b67b.

--- mm/filemap.c
+++ mm/filemap.c
@@ -2323,7 +2323,7 @@ struct page *grab_cache_page_write_begin(struct address_space *mapping,
 repeat:
 	page = find_lock_page(mapping, index);
 	if (page)
-		goto found;
+		return page;
 
 	page = __page_cache_alloc(mapping_gfp_mask(mapping) & ~gfp_notmask);
 	if (!page)
@@ -2336,8 +2336,6 @@ repeat:
 			goto repeat;
 		return NULL;
 	}
-found:
-	wait_on_page_writeback(page);
 	return page;
 }
 EXPORT_SYMBOL(grab_cache_page_write_begin);
