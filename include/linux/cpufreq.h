bc4263820ac2756be160e56ef96f6c5afeb89d3f
Seongmin Park <r_data@naver.com>
2013-04-06 22:40:30 +0900

    Add Hyper governor (Thanks to Dorimanx) Signed-off-by: Seongmin Park <r_data@naver.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -364,6 +364,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_hyper;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hyper)
 #endif
 
 
