aed66b11935d9f666f7346b5b59e7a7851a83bd0
Willi Ye <williye97@gmail.com>
2013-06-28 19:29:56 +0200

    cpufreq: hyper tuning

--- drivers/cpufreq/cpufreq_hyper.c
+++ drivers/cpufreq/cpufreq_hyper.c
@@ -50,7 +50,7 @@
 #define MAX_FREQUENCY_UP_THRESHOLD              (100)
 #define FREQ_STEP                               (40)
 #define UP_THRESHOLD_AT_MIN_FREQ                (40)
-#define FREQ_FOR_RESPONSIVENESS                 (800000)
+#define FREQ_FOR_RESPONSIVENESS                 (795000)
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #define DEF_SUSPEND_FREQ			(700000)
