76b163b2d721ac928b64b790a47fc99d8c6daa8c
Amperific <amperific@gmail.com>
2013-08-17 18:25:28 +0800

    mm/hotplug: remove unnecessary BUG_ON in __offline_pages()
    I think we can remove "BUG_ON(start_pfn >= end_pfn)" in __offline_pages(),
    because in memory_block_action() "nr_pages = PAGES_PER_SECTION * sections_per_block"
    is always greater than 0.
    
    memory_block_action()
    	offline_pages()
    		__offline_pages()
    			BUG_ON(start_pfn >= end_pfn)
    
    In v2.6.32, If info->length==0, this way may hit this BUG_ON().
    acpi_memory_disable_device()
    	remove_memory(info->start_addr, info->length)
    			offline_pages()
    
    A later Fujitsu patch renamed this function and the BUG_ON() is
    unnecessary.
    
    Signed-off-by: Xishi Qiu <qiuxishi@huawei.com>
    Reviewed-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Toshi Kani <toshi.kani@hp.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Amperific <amperific@gmail.com>

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -822,7 +822,6 @@ static int __ref offline_pages(unsigned long start_pfn,
 	struct zone *zone;
 	struct memory_notify arg;
 
-	BUG_ON(start_pfn >= end_pfn);
 	/* at least, alignment against pageblock is necessary */
 	if (!IS_ALIGNED(start_pfn, pageblock_nr_pages))
 		return -EINVAL;
