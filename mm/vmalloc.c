de40362818090862b304b339ef576392c1152783
Kostas K <mpokwsths@gmail.com>
2013-10-01 15:02:21 +0300

    Fix build error

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1045,7 +1045,7 @@ void vm_unmap_aliases(void)
 
 				j = find_last_bit(vb->dirty_map,
 +							VMAP_BBMAP_BITS);
-+				j = j + 1; /* need exclusive index */
++				j++; /* need exclusive index */
 
 				s = vb->va->va_start + (i << PAGE_SHIFT);
 				e = vb->va->va_start + (j << PAGE_SHIFT);
@@ -1981,7 +1981,7 @@ long vread(char *buf, char *addr, unsigned long count)
 	read_lock(&vmlist_lock);
 	for (tmp = vmlist; count && tmp; tmp = tmp->next) {
 		vaddr = (char *) tmp->addr;
-		if (addr >= vaddr + get_vm_area_size(tmp)
+		if (addr >= vaddr + get_vm_area_size(tmp))
 			continue;
 		while (addr < vaddr) {
 			if (count == 0)
