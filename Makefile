0a7de4b75a4ac4f824d8cb6861514da2b26f8dfc
Grarak <williye97@gmail.com>
2013-10-26 02:13:22 +0200

    remove dualboot ramdisk
    Signed-off-by: Grarak <williye97@gmail.com>

--- Makefile
+++ Makefile
@@ -192,7 +192,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?= arm
-CROSS_COMPILE	?= /home/grarak/android/kernel/toolchains/linaro-4.7-13.06/bin/arm-linux-androideabi-
+CROSS_COMPILE	?= /home/grarak/android/kernel/toolchain/linaro-2013.09-4.7/bin/arm-linux-androideabi-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
---
+++ build.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Colorize and add text parameters
+red=$(tput setaf 1) # red
+cya=$(tput setaf 6) # cyan
+
+txtbld=$(tput bold) # Bold
+
+bldred=${txtbld}$(tput setaf 1) # red
+bldcya=${txtbld}$(tput setaf 6) # cyan
+
+txtrst=$(tput sgr0) # Reset
+
+version=v1.3.0
+tcf=~/android/kernel/toolchains/
+
+DATE_START=$(date +"%s")
+
+##########################################################################
+echo -e "${bldcya}Do you want to clean up? ${txtrst} [N/y]"
+read cleanup
+
+if [ "$cleanup" == "y" ]; then
+        echo -e "Complete Clean? [N/y]"
+        read cleanoption
+
+        if [ "$cleanoption" == "n" ] || [ "$cleanoption" == "N" ]; then
+                echo -e "${bldcya}make clean ${txtrst}"
+        	make clean
+        fi
+
+        if [ "$cleanoption" == "y" ]; then
+                echo -e "${bldcya}make clean mrproper ${txtrst}"
+	        make clean mrproper
+        fi
+fi
+###########################################################################
+
+###########################################################################
+if [ -e .version ]; then
+	rm .version
+fi
+
+echo -e "${bldcya}Do you want to edit the kernel version? ${txtrst} [N/y]"
+read kernelversion
+
+if [ "$kernelversion" == "y" ]; then
+        echo -e "${bldcya}What version has your kernel? ${txtrst}"
+        echo "${bldred}NUMBERS ONLY! ${txtrst}"
+        read version
+ 
+        echo $version >> .version
+fi
+###########################################################################
+
+make tuna_defconfig
+
+cp arch/arm/configs/gk_tuna_defconfig .config
+sed -i s/CONFIG_LOCALVERSION=\".*\"/CONFIG_LOCALVERSION=\"-GraKernel_${butterversion}\"/ .config
+
+###########################################################################
+echo -e "${bldcya}This could take a while .... ${txtrst}"
+make -j4
+
+###########################################################################
+if [ -e arch/arm/boot/zImage ]; then
+
+	if [ -d romswitcher ]; then
+		cd romswitcher
+		git pull
+		cd ..
+	else
+		git clone git@github.com:RomSwitchers/RomSwitcher-tuna.git -b master romswitcher
+	fi
+
+	find -name "zImage" -exec cp -vf {} romswitcher/ \;
+	find -name "*.ko" -exec cp -vf {} romswitcher/boot.img-ramdisk/lib/modules/ \;
+
+	cd romswitcher
+
+	./build.sh
+   
+        echo -e "${bldcya} Finished!! ${txtrst}"
+        DATE_END=$(date +"%s")
+        DIFF=$(($DATE_END - $DATE_START))
+        echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+        date '+%a, %d %b %Y %H:%M:%S'
+
+else
+	echo "${bldred} KERNEL DID NOT BUILD! ${txtrst}"
+fi
+
+exit 0
+############################################################################
