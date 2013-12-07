ce181a26c9a39f9b40bd4dad7c96e602fef94d9e
Ian McKellar <ianloic@google.com>
2013-12-06 16:27:19 -0800

    Force McBSP I2S to use no data delays so that it matches ICS behavior.
    Also, name omap_mcbsp dai driver for future debugging sanity.
    
    Change-Id: I59d55eec27a8ba1177ef5dae6b7f541e5915e8c1

--- sound/soc/omap/omap-mcbsp.c
+++ sound/soc/omap/omap-mcbsp.c
@@ -411,9 +411,15 @@ static int omap_mcbsp_dai_set_dai_fmt(struct snd_soc_dai *cpu_dai,
 
 	switch (fmt & SND_SOC_DAIFMT_FORMAT_MASK) {
 	case SND_SOC_DAIFMT_I2S:
+#ifdef CONFIG_MACH_NOTLE
+		/* 0-bit data delay */
+		regs->rcr2      |= RDATDLY(0);
+		regs->xcr2      |= XDATDLY(0);
+#else
 		/* 1-bit data delay */
 		regs->rcr2	|= RDATDLY(1);
 		regs->xcr2	|= XDATDLY(1);
+#endif
 		break;
 	case SND_SOC_DAIFMT_LEFT_J:
 		/* 0-bit data delay */
@@ -610,6 +616,7 @@ static int omap_mcbsp_remove(struct snd_soc_dai *dai)
 }
 
 static struct snd_soc_dai_driver omap_mcbsp_dai = {
+	.name = "omap_mcbsp_dai",
 	.probe = omap_mcbsp_probe,
 	.remove = omap_mcbsp_remove,
 	.playback = {
