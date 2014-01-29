0ec3e093c8970a30aad7bf5799faa6575e36c5a2
Willi Ye <williye97@gmail.com>
2014-01-29 22:31:59 +0100

    mm: missing static
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- mm/page_io.c
+++ mm/page_io.c
@@ -42,7 +42,7 @@ static struct bio *get_swap_bio(gfp_t gfp_flags,
 	return bio;
 }
 
-void end_swap_bio_read(struct bio *bio, int err)
+static void end_swap_bio_write(struct bio *bio, int err)
 {
 	const int uptodate = test_bit(BIO_UPTODATE, &bio->bi_flags);
 	struct page *page = bio->bi_io_vec[0].bv_page;
