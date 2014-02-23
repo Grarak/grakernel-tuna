84cc1ba4b5226cca8347b1eca915615cee95bc29
Willi Ye <williye97@gmail.com>
2014-02-23 13:10:14 +0100

    omap2: show gpu clock in KHz
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -651,7 +651,7 @@ static struct freq_attr omap_UV_mV_table = {
 #endif
 
 static ssize_t show_gpu_clock(struct cpufreq_policy *policy, char *buf) {
-	return sprintf(buf, "%lu MHz\n", clk_get(NULL, "dpll_per_m7x2_ck")->rate/1000000);
+	return sprintf(buf, "%lu\n", clk_get(NULL, "dpll_per_m7x2_ck")->rate/1000);
 }
 
 static struct freq_attr gpu_clock = {
