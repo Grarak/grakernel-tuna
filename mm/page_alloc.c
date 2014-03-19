25a566d265fb423e3182a23845a5e754fabb9906
Kai Yick <kingkai@google.com>
2014-03-19 13:42:18 -0700

    Increase vm.min_free_kbytes for NOTLE from 8M -> 16M.
    This should help in situation where we need a burst of allocations
    before the kernel has time to free buffers and caches.
    b/12852171
    
    Change-Id: I9bed02dcbf50d53eeb1a252191d74803aa386e62

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5194,8 +5194,8 @@ int __meminit init_per_zone_wmark_min(void)
 	if (min_free_kbytes < 128)
 		min_free_kbytes = 128;
 #ifdef CONFIG_MACH_NOTLE
-	if (min_free_kbytes < 8192)
-		min_free_kbytes = 8192;
+	if (min_free_kbytes < 16384)
+		min_free_kbytes = 16384;
 #else
 	if (min_free_kbytes < 128)
 		min_free_kbytes = 128;
